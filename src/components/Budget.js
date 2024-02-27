8bc4c28eaeaef8bcbf55025d703a86b58f238e8e
Icelycoool <mohammedcali@proton.me>
2024-02-27 17:51:58 -0500

    Final react lab

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,28 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const Budget = () => {
-    const { budget } = useContext(AppContext);
+const Budget = ({ currency }) => { // Receive currency as a prop
+    const { budget, spending } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        let value = parseInt(event.target.value);
+        if (value > 20000) {
+            setNewBudget(20000);
+            alert("Budget cannot exceed £20,000.");
+        } else if (value < spending) {
+            alert("Budget cannot be lower than spending.");
+        } else {
+            setNewBudget(value);
+        }
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency} {budget}</span>
+                <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} />
+        </div>
     );
 };
+
 export default Budget;
---
+++ src/components/CurrencyDropdown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CurrencyDropdown = ({ value, onChange }) => {
+    return (
+        <select className='form-select' value={value} onChange={onChange}>
+            <option value="£">Currency: £ (Pound)</option>
+            <option value="$">Currency: $ (Dollar)</option>
+            <option value="€">Currency: € (Euro)</option>
+            <option value="₹">Currency: ₹ (Rupee)</option>
+        </select>
+    );
+};
+
+export default CurrencyDropdown;
