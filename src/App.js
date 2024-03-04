793f573b3751e072f00fdbaa0f20f1b2d880c563
Icelycoool <mohammedcali@proton.me>
2024-03-05 00:47:00 +0300

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { AppProvider } from './context/AppContext';
-import Budget from './components/Budget';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import AllocationForm from './components/AllocationForm';
-import RemainingBudget from './components/Remaining';
-import CurrencyDropdown from './components/CurrencyDropdown'; // Import the new component
+import { AppProvider } from "./context/AppContext";
+import Budget from "./components/Budget";
+import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AllocationForm from "./components/AllocationForm";
+import RemainingBudget from "./components/Remaining";
+import CurrencyDropdown from "./components/CurrencyDropdown";
 
 const App = () => {
-    const [currency, setCurrency] = useState('£'); // Default currency is Pound
+	const [currency, setCurrency] = useState("£");
 
-    const handleCurrencyChange = (event) => {
-        setCurrency(event.target.value);
-    }
+	const handleCurrencyChange = (event) => {
+		setCurrency(event.target.value);
+	};
 
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
-                    <div className='col-sm'>
-                    <Budget currency={currency} />
-                    </div>
-                    <div className='col-sm'>
-                        <RemainingBudget />
-                    </div>
-                    <div className='col-sm'>
-                        <ExpenseTotal />
-                    </div>
-                    <div className='col-sm'>
-                            <label htmlFor='currency' className='form-label'>Currency: </label>
-                            <CurrencyDropdown value={currency} onChange={handleCurrencyChange} />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Allocation</h3>
-                <div className='row '>
-                    <div className='col-sm'>
-                        <ExpenseList />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Change allocation</h3>
-                <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <AllocationForm/>
-                    </div>
-                </div>
-            </div>
-        </AppProvider>
-    );
+	return (
+		<AppProvider>
+			<div className="container">
+				<h1 className="mt-3">Company's Budget Allocation</h1>
+				<div className="row mt-3">
+					<div className="col-sm">
+						<Budget currency={currency} />
+					</div>
+					<div className="col-sm">
+						<RemainingBudget />
+					</div>
+					<div className="col-sm">
+						<ExpenseTotal />
+					</div>
+					<div className="col-sm">
+						<CurrencyDropdown
+							value={currency}
+							onChange={(handleCurrencyChange, setCurrency)}
+						/>
+					</div>
+				</div>
+				<h3 className="mt-3">Allocation</h3>
+				<div className="row ">
+					<div className="col-sm">
+						<ExpenseList />
+					</div>
+				</div>
+				<h3 className="mt-3">Change allocation</h3>
+				<div className="row mt-3">
+					<div className="col-sm">
+						<AllocationForm currency={currency} />
+					</div>
+				</div>
+			</div>
+		</AppProvider>
+	);
 };
 
 export default App;
--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
+import "../App.css";
 
 const CurrencyDropdown = ({ value, onChange }) => {
-    return (
-        <select className='form-select' value={value} onChange={onChange}>
-            <option value="£">Currency: £ (Pound)</option>
-            <option value="$">Currency: $ (Dollar)</option>
-            <option value="€">Currency: € (Euro)</option>
-            <option value="₹">Currency: ₹ (Rupee)</option>
-        </select>
-    );
+	return (
+		<select
+			className="form-select"
+			value={value}
+			onChange={onChange}>
+			<option value="£">Currency (£ Pound)</option>
+			<option value="$">Currency ($ Dollar)</option>
+			<option value="€">Currency (€ Euro)</option>
+			<option value="₹">Currency (₹ Rupee)</option>
+		</select>
+	);
 };
 
 export default CurrencyDropdown;
